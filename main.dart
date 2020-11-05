1bf6d8b8b8536e3f2a5860a135dec2be289442fe
pkmangukiya <45510447+pkmangukiya@users.noreply.github.com>
2020-11-05 15:28:47 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:test_demo/swipedetector.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: SwipeDetector(
+          child: Container(height: 200, width: 300, color: Colors.red),
+          onSwipeUp: () {
+            print("Swipe Up");
+          },
+          onSwipeDown: () {
+           print("Swipe Down");
+          },
+          onSwipeLeft: () {
+            print("Swipe Left");
+          },
+          onSwipeRight: () {
+            print("Swipe Right");
+          },
+        ),
+      ),
+    );
+  }
+}
